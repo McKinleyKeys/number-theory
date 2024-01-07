8d1ba4378634a524605e35b836c80f206c99acc6
McKinley Keys <mckinley.keys@gmail.com>
2024-01-08 08:53:10 +1100

    Make the `if_true'` family work for arbitrary types

--- Playground/Logic.lean
+++ Playground/Logic.lean
@@ -1,25 +1,25 @@
 
 import Mathlib
 
-lemma if_true' {c : Prop} [Decidable c] {t : ℕ} {e : ℕ} (h : c) :
+lemma if_true' {c : Prop} [Decidable c] {t : α} {e : α} (h : c) :
   (if c then t else e) = t
   := by
     apply (ite_eq_left_iff (P := c)).mpr
     intro hc
     contradiction
-lemma if_false' {c : Prop} [Decidable c] {t : ℕ} {e : ℕ} (h : ¬c) :
+lemma if_false' {c : Prop} [Decidable c] {t : α} {e : α} (h : ¬c) :
   (if c then t else e) = e
   := by
     rw [← ite_not c, if_true' h]
 
-lemma dite_true' {c : Prop} [Decidable c] {t : c → ℕ} {e : ¬c → ℕ} (h : c) :
+lemma dite_true' {c : Prop} [Decidable c] {t : c → α} {e : ¬c → α} (h : c) :
   (if hc : c then t hc else e hc) = t h
   := by
     change (if hc : c then t h else e hc) = t h
     apply (dite_eq_left_iff (P := c)).mpr
     intro hc
     contradiction
-lemma dite_false' {c : Prop} [Decidable c] {t : c → ℕ} {e : ¬c → ℕ} (h : ¬c) :
+lemma dite_false' {c : Prop} [Decidable c] {t : c → α} {e : ¬c → α} (h : ¬c) :
   (if hc : c then t hc else e hc) = e h
   := by
     change (if hc : c then t hc else e h) = e h
